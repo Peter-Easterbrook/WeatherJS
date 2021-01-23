5b8fa4970344d8190513aaa789744961af1277da
Peter-Easterbrook <peter.easterbrook@gmail.com>
2021-01-23 19:18:05 +0100

    23.1.2021
    Changed default city

--- storage.js
+++ storage.js
@@ -2,8 +2,8 @@ class Storage {
   constructor() {
     this.city;
     this.country;
-    this.defaultCity = 'London';
-    this.defaultState = 'UK';
+    this.defaultCity = 'Vienna';
+    this.defaultState = 'Austria';
   }
 
   getLocationData() {
